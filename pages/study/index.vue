3fd2f2a1f3e016d3f7d350d7bc21a3aa5ca6eb3f
lights0123 <developer@lights0123.com>
2018-12-22 17:47:32 -0500

    Initial Commit

---
+++ pages/study/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="container">
+    <el-container>
+      <el-header>
+        <h1>Select A Course</h1>
+      </el-header>
+      <el-main>
+        <el-row>
+          <el-col v-for="course in courses" :key="course.url" :span="24 / 4">
+            <div class="grid-content"><el-button class="button" @click="$router.push(`/study/${course.url}`)">{{course.name}}</el-button></div>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Index extends Vue {
+	public courses = [{ name: 'Physical Science', url: 'physical-science' }];
+}
+</script>
+
+<style scoped>
+.grid-content >>> button {
+    width: 100%;
+}
+</style>
---
+++ pages/study/physical-science.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container">
+    <el-container>
+      <el-header>
+        <h1>Physical Science</h1>
+      </el-header>
+      <el-main>
+        <el-row>
+          <el-col v-for="course in courses" :key="course.url" :span="24 / 4">
+            <div class="grid-content"><el-button>{{course.name}}</el-button></div>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Physci extends Vue {}
+</script>
