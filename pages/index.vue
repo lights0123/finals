90a86b7cf76d5296c0a1945a4e006a8d08967450
lights0123 <developer@lights0123.com>
2018-12-23 12:26:35 -0500

    Use tabs

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <section class="container">
-    <el-container>
-      <el-header>
-        <h1>Select A Course</h1>
-      </el-header>
-      <el-main>
-        <el-row>
-          <el-col v-for="course in courses" :key="course.url" :xl="24 / 4" :lg="24 / 4" :md="24 / 4" :sm="24 / 2" :xs="24">
-            <div class="grid-content"><el-button class="button" @click="$router.push(`/study/${course.url}`)">{{course.name}}</el-button></div>
-          </el-col>
-        </el-row>
-      </el-main>
-    </el-container>
-  </section>
+	<section class="container">
+		<el-container>
+			<el-header>
+				<h1>Select A Course</h1>
+			</el-header>
+			<el-main>
+				<el-row>
+					<el-col v-for="course in courses" :key="course.url" :xl="24 / 4" :lg="24 / 4" :md="24 / 4" :sm="24 / 2" :xs="24">
+						<div class="grid-content"><el-button class="button" @click="$router.push(`/study/${course.url}`)">{{course.name}}</el-button></div>
+					</el-col>
+				</el-row>
+			</el-main>
+		</el-container>
+	</section>
 </template>
 
 <script lang="ts">
@@ -26,6 +26,6 @@ export default class Index extends Vue {
 
 <style scoped>
 .grid-content >>> button {
-    width: 100%;
+		width: 100%;
 }
 </style>
